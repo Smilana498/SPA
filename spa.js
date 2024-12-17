bb30684f8543dc00cad5c48eeb0d9db3bacd3295
Smilana498 <mm6085058@gmail.com>
2024-12-17 12:38:10 +0600

    Add files via upload

---
+++ spa.js
@@ -0,0 +1,11 @@
+document.getElementById("MyButton").addEventListener("click", function (){
+    window.location.href="https://wa.me//996222629405"
+})
+
+document.getElementById("MyButto").addEventListener("click", function (){
+    window.location.href="https://wa.me//996222629405"
+})
+
+document.getElementById("MyButt").addEventListener("click", function (){
+    window.location.href="https://wa.me//996222629405"
+})
